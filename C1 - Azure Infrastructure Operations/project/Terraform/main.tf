b30c839d7dda130d708d0c8b4102b8b42432e1bf
MartinHappyman <marcosmarkmaster@gmail.com>
2023-02-16 20:11:42 +0100

    terraform

--- C1 - Azure Infrastructure Operations/project/Terraform/main.tf
+++ C1 - Azure Infrastructure Operations/project/Terraform/main.tf
@@ -2,68 +2,187 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-resources"
+resource "azurerm_resource_group" "project" {
+  name     = "${var.prefix}-RG"
   location = var.location
-}
 
-resource "azurerm_virtual_network" "example" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "${var.prefix}-PublicIP"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  allocation_method   = "Static"
-
-  tags = {
-    environment = "Test"
+    tags = {
+    Project = "${var.Project}"
   }
 }
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
+
+resource "azurerm_virtual_network" "project" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.project.location
+  resource_group_name = azurerm_resource_group.project.name
+
+    tags = {
+    Project = "${var.Project}"
+  }
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "${var.prefix}-nic"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_subnet" "internal" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.project.name
+  virtual_network_name = azurerm_virtual_network.project.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+}
+
+resource "azurerm_public_ip" "project" {
+ # count = var.vm_count
+ # name                = "${var.prefix}-PublicIP_${count.index}"
+  name                = "${var.prefix}-PublicIP_$"
+  resource_group_name = azurerm_resource_group.project.name
+  location            = azurerm_resource_group.project.location
+  allocation_method   = "Static"
+
+    tags = {
+    Project = "${var.Project}"
+  }
+}
+
+resource "azurerm_network_interface" "project" {
+ # count               = var.vm_count
+ # name                = "${var.prefix}-nic_${count.index}"
+   name                = "${var.prefix}-nic"
+  resource_group_name = azurerm_resource_group.project.name
+  location            = azurerm_resource_group.project.location
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.project.id
+  }
+  tags = {
+  Project = "${var.Project}"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "example" {
-  name                            = "${var.prefix}-vm"
-  resource_group_name             = azurerm_resource_group.example.name
-  location                        = azurerm_resource_group.example.location
-  size                            = "Standard_D2s_v3"
-  admin_username                  = "${var.username}"
-  admin_password                  = "${var.password}"
-  disable_password_authentication = false
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
+resource "azurerm_network_security_group" "project" {
+  name                = "${var.prefix}-nsg"
+  location            = azurerm_resource_group.project.location
+  resource_group_name = azurerm_resource_group.project.name
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
+  security_rule {
+    name                       = "AllowVNetInbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "VirtualNetwork"
   }
 
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
+  security_rule {
+    name                       = "DenyInternetInbound"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
+
+  tags = {
+  Project = "${var.Project}"
+  }
+}
+
+resource "azurerm_lb" "project" {
+ # count = var.vm_count
+  name                = "${var.prefix}-lb"
+  location            = azurerm_resource_group.project.location
+  resource_group_name = azurerm_resource_group.project.name
+
+frontend_ip_configuration {
+    name                          = "${var.prefix}-feip"
+    public_ip_address_id          = azurerm_public_ip.project.id
+}
+}
+
+resource "azurerm_lb_backend_address_pool" "project" {
+  name                = "${var.prefix}-bap"
+  loadbalancer_id     = azurerm_lb.project.id
+}
+
+resource "azurerm_lb_probe" "project" {
+    name                      = "${var.prefix}-probe"
+    protocol                  = "Tcp"
+    port                      = 80
+    loadbalancer_id           = azurerm_lb.project.id
+}
+
+resource "azurerm_lb_rule" "project" {
+    name                           = "${var.prefix}-lbr"
+    protocol                       = "Tcp"
+    frontend_port                  = 80
+    backend_port                   = 80
+    frontend_ip_configuration_name = azurerm_lb.project.frontend_ip_configuration[0].name
+    probe_id                       = azurerm_lb_probe.project.id
+    loadbalancer_id                = azurerm_lb.project.id
+}
+
+
+resource "azurerm_network_interface_backend_address_pool_association" "project" {
+  network_interface_id    = azurerm_network_interface.project.id
+  ip_configuration_name   = azurerm_network_interface.project.ip_configuration.0.name
+  backend_address_pool_id = azurerm_lb_backend_address_pool.project.id
+}
+
+resource "azurerm_availability_set" "project" {
+  name                = "${var.prefix}-aset"
+  location            = azurerm_resource_group.project.location
+  resource_group_name = azurerm_resource_group.project.name
+  platform_fault_domain_count = 2
+  tags = {
+  Project = "${var.Project}"
+  }
+}
+
+data "azurerm_resource_group" "image" {
+  name                = var.packer_resource_group_name
+}
+
+data "azurerm_image" "image" {
+  name                = var.packer_image_name
+  resource_group_name = data.azurerm_resource_group.image.name
+}
+
+resource "azurerm_virtual_machine" "project" {
+ # count                  = var.vm_count
+ # name                   = "UbuntuVM_${count.index}"
+   name                   = "UbuntuVM"
+  location               = azurerm_resource_group.project.location
+  resource_group_name    = azurerm_resource_group.project.name
+  network_interface_ids  = ["${azurerm_network_interface.project.id}"]
+  vm_size                = "Standard_DS1_v2"
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    id = "${data.azurerm_image.image.id}"
+  }
+
+  storage_os_disk {
+    name              = ""
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+}
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+}
+
+  tags = {
+  Project = "${var.Project}"
+  }
+
 }
--- C1 - Azure Infrastructure Operations/project/Terraform/variables.tf
+++ C1 - Azure Infrastructure Operations/project/Terraform/variables.tf
@@ -4,13 +4,23 @@ variable "prefix" {
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
-  default     = "East US"
+  default     = "Switzerland North"
 }
 
-variable "username" {
-  description = "Username od admin"
+variable "Project" {
+  description = "Tag indicating which project the resource belongs to"
 }
 
-variable "password" {
-  description = "Password of admin"
+variable "packer_resource_group_name" {
+   description = "Name of the resource group in which the Packer image will be created"
+   default     = "Udacity-demo-rg"
+}
+
+variable "packer_image_name" {
+   description = "Name of the Packer image"
+   default     = "Ubuntu4_Image"
+}
+
+variable "vm_count" {
+   description = "Number of Virtual Machines"
 }
